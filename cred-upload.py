bf5d5b8b2068e8c2b4dbd68c577186039a070d22
jamessmith <jamessmith>
2019-12-18 13:06:43 -0500

    Initial commit

---
+++ cred-upload.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#
+# SAMPLE SCRIPT ONLY
+# THIS IS NOT SUPPORTED BY TENABLE.
+# THIS IS NOT SUPPORTED BY ANYONE.
+#
+# This shows how to take an SSH private key and upload it to Tenable.io and create a new credential entry from it.
+#
+# Requires the following:
+#   pip install pytenable
+
+import os
+import json
+from tenable.io import TenableIO
+access_key=os.getenv('TIO_ACCESS_KEY')
+secret_key=os.getenv('TIO_SECRET_KEY')
+tio = TenableIO(access_key, secret_key)
+respraw = tio.post('credentials/files', files={'Filedata': open('private_rsa_key', 'rb')})
+resp=json.loads(respraw.text)
+print("Response:",respraw.text)
+if "fileuploaded" in resp:
+    cred_uuid = tio.credentials.create("Test creds with private key auth", "SSH", auth_method="public key", elevate_privileges_with="Nothing", username="root",private_key=resp["fileuploaded"])
+
+print("Credential UUID:",cred_uuid)
+
